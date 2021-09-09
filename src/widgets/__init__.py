f65ab1ba0479392a1d79bbd2ec414a3f0c2cd4d5
virgilsisoe <sisoe24@gmail.com>
2021-09-09 17:43:51 -0400

    change order of imports

--- src/widgets/__init__.py
+++ src/widgets/__init__.py
@@ -1,5 +1,5 @@
-from .text_widgets import TextWidgets
-from .server_status import ServerStatus
 from .error_dialog import ErrorDialog
 from .toolbar import ToolBar
+from .server_status import ServerStatus
+from .text_widgets import TextWidgets
 from .script_editor import FakeScriptEditor
