b24951034cfa018f15922fe1e2aa2109c0dd06dc
virgilsisoe <sisoe24@gmail.com>
2021-09-18 12:46:21 -0400

    changed file name

--- src/widgets/__init__.py
+++ src/widgets/__init__.py
@@ -2,4 +2,4 @@ from .error_dialog import ErrorDialog
 from .toolbar import ToolBar
 from .server_status import ServerStatus
 from .text_widgets import TextWidgets
-from .script_editor import FakeScriptEditor
+from .fake_script_editor import FakeScriptEditor
