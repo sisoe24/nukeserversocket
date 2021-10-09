f34b7ba7522a5f7af5033cd2a5a30d8ec3d281e1
virgilsisoe <sisoe24@gmail.com>
2021-10-08 20:57:49 -0400

    update testing

--- tests/test_settings_widget.py
+++ tests/test_settings_widget.py
@@ -1,47 +1,32 @@
 import os
 import configparser
-
+from typing import List
 import pytest
 from PySide2.QtWidgets import QCheckBox, QWidget
 
 from src.widgets import settings_widget
 
 
-def test_no_settings_default_values(setup_no_settings):
-    """Check if checkbox values are on the default if no ini file is found."""
-    initial_values = {
-        'output_to_console': True,
-        'format_text': True,
-        'clear_output': True,
-        'override_input_editor': False,
-        'show_file_path': False,
-        'show_unicode': True,
-    }
-    widget: QWidget = settings_widget.SettingsWidget()
-    checkboxes: QCheckBox = widget.findChildren(QCheckBox)
-
-    checkbox: QCheckBox
-    for checkbox in checkboxes:
-        value = initial_values[checkbox.objectName()]
-        assert checkbox.isChecked() == value
-
-
 class TestSubStateSettings:
+    settings_widget: QWidget
 
     @property
-    def output_console(self):
+    def output_console(self) -> QCheckBox:
         return self.settings_widget._output_console
 
     @property
-    def format_output(self):
+    def format_output(self) -> QCheckBox:
         return self.settings_widget._format_output
 
     @property
-    def clear_text(self):
+    def clear_text(self) -> QCheckBox:
         return self.settings_widget._clear_output
 
     def test_output_console_is_true(self, setup_no_settings):
-        self.settings_widget: QWidget = settings_widget.SettingsWidget()
+        """If `console_output` is enable, so it should be for `clear_text` and 
+        `format_output`.
+        """
+        self.settings_widget = settings_widget.SettingsWidget()
 
         self.output_console.setChecked(True)
 
@@ -58,7 +43,7 @@ class TestSubStateSettings:
             assert not self.clear_text.isEnabled()
             assert not self.format_output.isEnabled()
 
-        self.settings_widget: QWidget = settings_widget.SettingsWidget()
+        self.settings_widget = settings_widget.SettingsWidget()
         self.output_console.setChecked(False)
 
         is_sub_state_false()
@@ -74,7 +59,7 @@ class TestSubStateSettings:
             assert not self.clear_text.isEnabled()
             assert not self.clear_text.isChecked()
 
-        self.settings_widget: QWidget = settings_widget.SettingsWidget()
+        self.settings_widget = settings_widget.SettingsWidget()
         self.output_console.setChecked(True)
         self.format_output.setChecked(False)
 
@@ -85,32 +70,52 @@ class TestSubStateSettings:
         is_sub_state_false()
 
 
+class TestSettingsFile:
+    settings_widget: QWidget
 
-@pytest.fixture()
-def setup_fake_config_file(setup_no_settings):
-    """Write options state to fake config file"""
+    @property
+    def checkboxes(self) -> List[QCheckBox]:
+        return self.settings_widget.findChildren(QCheckBox)
 
-    widget: QWidget = settings_widget.SettingsWidget()
-    checkboxes: QCheckBox = widget.findChildren(QCheckBox)
+    def test_no_settings_default_values(self, setup_no_settings):
+        """Check if checkbox values are on the default if no ini file is found."""
+        initial_values = {
+            'output_to_console': True,
+            'format_text': True,
+            'clear_output': True,
+            'override_input_editor': False,
+            'show_file_path': False,
+            'show_unicode': True,
+        }
+        self.settings_widget = settings_widget.SettingsWidget()
 
-    checkbox: QCheckBox
-    for checkbox in checkboxes:
-        checkbox.toggle()
-        checkbox.setChecked(True)
+        checkbox: QCheckBox
+        for checkbox in self.checkboxes:
+            value = initial_values[checkbox.objectName()]
+            assert checkbox.isChecked() == value
 
+    @pytest.fixture()
+    def setup_fake_config_file(self, setup_no_settings):
+        """Write options state to fake config file"""
 
-def test_settings_exists(setup_fake_config_file, fake_settings_file):
-    """Check if file settings was created."""
-    assert os.path.exists(fake_settings_file)
+        self.settings_widget = settings_widget.SettingsWidget()
 
+        checkbox: QCheckBox
+        for checkbox in self.checkboxes:
+            checkbox.toggle()
+            checkbox.setChecked(True)
 
-def test_check_value(setup_fake_config_file, fake_settings_file):
-    """Check if file checkboxes saved their state on the file settings"""
+    def test_settings_exists(self, setup_fake_config_file, fake_settings_file):
+        """Check if file settings was created."""
+        assert os.path.exists(fake_settings_file)
 
-    config = configparser.ConfigParser()
-    config.read(fake_settings_file)
+    def test_check_value(self, setup_fake_config_file, fake_settings_file):
+        """Check if file checkboxes saved their state on the file settings"""
 
-    settings_values = config['options']
+        config = configparser.ConfigParser()
+        config.read(fake_settings_file)
 
-    for opt in settings_values:
-        assert config.getboolean('options', opt)
+        settings_values = config['options']
+
+        for opt in settings_values:
+            assert config.getboolean('options', opt)
