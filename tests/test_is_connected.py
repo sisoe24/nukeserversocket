fd16fc01169adf151cf3482f7d2d1115836a0fab
virgilsisoe <sisoe24@gmail.com>
2021-10-11 16:58:52 -0400

    .

--- tests/test_is_connected.py
+++ tests/test_is_connected.py
@@ -4,11 +4,16 @@ import socket
 
 import pytest
 
+# from src.connection import SendNodesClient, SendTestClient
+
 STRING = 'nss test msg'
 TEXT = "print('%s'.upper())" % STRING
 FILE = 'tmp/path/nss.py'
 
 data = [TEXT, json.dumps({"text": TEXT, "file": FILE})]
+
+# TODO: should connect to a different port in case the usual port is already busy
+# TODO: need second instance to test if already connected
 
 
 @pytest.fixture(params=data)
@@ -42,5 +47,35 @@ class TestConnectedLogs:
     def test_message_output(self, qtbot, ui):
         def check_status():
             assert STRING.upper() in ui.log_widgets.output_text
+
+        qtbot.waitUntil(check_status)
+
+
+@pytest.mark.usefixtures('start_connection')
+class TestSendTestMsg:
+
+    def test_message_status(self, qtbot, ui):
+        ui._send_test()
+
+        def check_status():
+            assert 'Connection successful' in ui.log_widgets.status_text
+
+        qtbot.waitUntil(check_status)
+
+    def test_message_received(self, qtbot, ui):
+        ui._send_test()
+
+        def check_status():
+            msg = "from __future__ import print_function; print('Hello from Test Client'"
+            assert msg in ui.log_widgets.received_text
+
+        qtbot.waitUntil(check_status)
+
+    def test_message_output(self, qtbot, ui):
+        ui._send_test()
+
+        def check_status():
+            msg = "Hello from Test Client"
+            assert msg in ui.log_widgets.output_text
 
         qtbot.waitUntil(check_status)
