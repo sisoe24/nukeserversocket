ef51dd1b6da1d368abe5dc51df3335592790a653
virgilsisoe <sisoe24@gmail.com>
2021-10-16 16:49:29 -0400

    added timer for socket and better error message

--- src/connection/nss_socket.py
+++ src/connection/nss_socket.py
@@ -6,7 +6,7 @@ import logging
 
 from PySide2.QtCore import QObject, Signal
 
-from ..utils import validate_output
+from ..utils import validate_output, connection_timer
 from ..script_editor import CodeEditor
 
 LOGGER = logging.getLogger('NukeServerSocket.socket')
@@ -19,19 +19,26 @@ class Socket(QObject):
 
     def __init__(self, socket):
         QObject.__init__(self)
-        LOGGER.debug('Socket :: listening...')
+        LOGGER.debug('Socket :: Listening...')
 
         self.socket = socket
         self.socket.connected.connect(self.on_connected)
         self.socket.disconnected.connect(self.on_disconnected)
         self.socket.readyRead.connect(self.on_readyRead)
 
+        self.timer = connection_timer(30)
+        self.timer.timeout.connect(self.close_socket)
+        self.timer.start()
+
     def on_connected(self):
         LOGGER.debug('Client connect event')
-        self.state_changed.emit("Client Connected Event")
 
     def on_disconnected(self):
         LOGGER.debug('Socket :: Disconnected.')
+
+    def close_socket(self):
+        self.socket.close()
+        self.timer.stop()
 
     def _get_message(self):
         """Get the socket message.
@@ -58,25 +65,46 @@ class Socket(QObject):
         try:
             LOGGER.debug('Socket :: Message is stringified array.')
             msg_data = json.loads(msg.data())
+
+        # could raise json.decoder.JSONDecodeError when decoding problems
+        # but if other errors happens will break the script, and I would like
+        # to just "ignore" and not execute any code
         except Exception as err:
-            LOGGER.critical("Invalid Json Deserialization: %s",
-                            err, exc_info=True)
-            raise ValueError('Json Deserialization error')
+            msg = "Error json deserialization. %s: %s" % (err, msg.data())
+
+            LOGGER.critical(msg)
+            self.state_changed.emit(msg)
+
+            raise RuntimeError('Json Deserialization error')
+
         else:
             LOGGER.debug('Socket :: Message received: %s', msg_data)
             return msg_data
 
+    @staticmethod
+    def _is_valid_data(msg_text):
+        """Check if data received is a valid string."""
+        return bool(msg_text and isinstance(msg_text, str) and not msg_text.isspace())
+
     def on_readyRead(self):
         LOGGER.debug('Socket :: Message ready')
+        self.timer.start()
 
         try:
             msg_data = self._get_message()
-        except ValueError:
+        except RuntimeError:
+            self.close_socket()
             return
 
-        msg_text = msg_data.get('text')
-        if not msg_text:
-            LOGGER.warning("no text data to execute")
+        msg_text = msg_data.get('text')  # type: str
+
+        if not self._is_valid_data(msg_text):
+            msg = 'Error. Invalid data: %s' % msg_data
+
+            LOGGER.warning('Socket :: %s', msg)
+            self.state_changed.emit(msg)
+
+            self.close_socket()
             return
 
         editor = CodeEditor(msg_data.get('file', ''))
@@ -90,7 +118,7 @@ class Socket(QObject):
         LOGGER.debug('Socket :: sending message back.')
         self.socket.write(validate_output(output_text))
 
-        self.socket.close()
+        self.close_socket()
 
         self.received_text.emit(msg_text)
         self.output_text.emit(output_text)
