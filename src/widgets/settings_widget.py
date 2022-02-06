6a1e89f0604e2b66c80a3f7397f93af9ff73c84a
virgilsisoe <virgilsisoe@gmail.com>
2022-02-06 14:30:03 -0500

    update settings widget

--- src/widgets/settings_widget.py
+++ src/widgets/settings_widget.py
@@ -7,7 +7,6 @@ import logging
 from PySide2.QtWidgets import (
     QCheckBox,
     QFormLayout,
-    QLabel,
     QWidget,
     QGroupBox,
     QVBoxLayout
@@ -26,14 +25,14 @@ class CheckBox(QCheckBox):
     connect the toggle signal to update the configuration setting value.
     """
 
-    def __init__(self, title, label, is_checked, tooltip, parent=None):
+    def __init__(self, title, label, default_state, tooltip, parent=None):
         # type: (str, str, bool, str, QWidget | None) -> None
         """Init method for the CheckBox class.
 
         Args:
             title (str): title of the checkbox.
             label (str): label of the checkbox.
-            is_checked (bool): initial state of the checkbox if does not have
+            default_state (bool): initial state of the checkbox if does not have
             already a setting value in the config file.
             tooltip (str): checkbox tooltip.
             parent (str, optional): QWidget to set as parent. Defaults to None.
@@ -43,14 +42,13 @@ class CheckBox(QCheckBox):
         self._label = label
 
         obj_name = title.lower().replace(' ', '_')
-        ini_value = 'options/%s' % obj_name
+        setting_name = 'options/%s' % obj_name
         self.setObjectName(obj_name)
 
-        self.settings = AppSettings()
-        self.setChecked(self.settings.get_bool(ini_value, is_checked))
-
+        settings = AppSettings()
+        self.setChecked(settings.get_bool(setting_name, default_state))
         self.toggled.connect(
-            lambda: self.settings.setValue(ini_value, self.isChecked())
+            lambda: settings.setValue(setting_name, self.isChecked())
         )
 
 
@@ -67,31 +65,30 @@ class SettingsWidget(QWidget):
         self.setObjectName('SettingsWidget')
 
         self._se_checkbox = QGroupBox('Mirror To Script Editor')
-        self._se_checkbox.setCheckable(True)
-        self._se_checkbox.setChecked(False)
+        self._setup_group_toggle()
 
         self._output_console = CheckBox(
-            is_checked=False, title='Override Output Editor', parent=self,
+            default_state=False, title='Override Output Editor', parent=self,
             tooltip='Output to internal Script Editor', label='Output:')
 
         self._format_output = CheckBox(
-            is_checked=False, title='Format Text', parent=self, label='',
+            default_state=False, title='Format Text', parent=self, label='',
             tooltip='Clean and format output console text')
 
         self._clear_output = CheckBox(
-            is_checked=False, title='Clear Output', parent=self, label='',
+            default_state=False, title='Clear Output', parent=self, label='',
             tooltip='Clear previous output in console. Works only if Format Text is enabled')
 
         self._show_path = CheckBox(
-            is_checked=False, title='Show File Path', parent=self, label='',
+            default_state=False, title='Show File Path', parent=self, label='',
             tooltip='Include full path of the executed file')
 
         self._show_unicode = CheckBox(
-            is_checked=False, title='Show Unicode', parent=self, label='',
+            default_state=False, title='Show Unicode', parent=self, label='',
             tooltip='include unicode character in output text')
 
         self._override_input = CheckBox(
-            is_checked=False, title='Override Input Editor', parent=self,
+            default_state=False, title='Override Input Editor', parent=self,
             tooltip='Override internal input text editor', label='Input:')
 
         _layout_checkboxes = QFormLayout()
@@ -110,6 +107,20 @@ class SettingsWidget(QWidget):
         self._se_checkbox.toggled.connect(self._toggle_sub_options)
         self._output_console.toggled.connect(self._toggle_output_options)
         self._format_output.toggled.connect(self._toggle_clear_console)
+
+    def _setup_group_toggle(self):
+        """Set initial default groupbox widget settings."""
+        self._se_checkbox.setCheckable(True)
+
+        setting_name = "options/mirror_to_script_editor"
+
+        settings = AppSettings()
+        self._se_checkbox.setChecked(settings.get_bool(setting_name))
+
+        self._se_checkbox.toggled.connect(
+            lambda: settings.setValue(
+                setting_name, self._se_checkbox.isChecked())
+        )
 
     @staticmethod
     def _toggle_checkboxes(checkbox, state):  # type: (CheckBox, bool) -> None
@@ -142,7 +153,6 @@ class SettingsWidget(QWidget):
         Args:
             state (bool): state of output_console attribute
         """
-
         self._toggle_checkboxes(self._format_output, state)
         self._toggle_checkboxes(self._clear_output, state)
         self._toggle_checkboxes(self._show_path, state)
