e1977efc4ac775740d8e2aea401aaa9a5f35ba7e
virgilsisoe <virgilsisoe@gmail.com>
2022-02-19 20:07:26 -0500

    new settings for the connection type

--- src/widgets/settings_widget.py
+++ src/widgets/settings_widget.py
@@ -3,6 +3,7 @@
 from __future__ import print_function
 
 import logging
+from collections import namedtuple
 
 from PySide2.QtCore import Qt
 from PySide2.QtWidgets import (
@@ -19,6 +20,8 @@ from PySide2.QtWidgets import (
 from ..utils import AppSettings
 
 LOGGER = logging.getLogger('NukeServerSocket.settings_widget')
+
+_RadioButton = namedtuple('RadioButtons', ['label', 'state'])
 
 
 def _format_name(name):
@@ -224,7 +227,7 @@ class TimeoutSettings(QGroupBox):
         return spinbox
 
 
-class CodeEngineSettings(QGroupBox):
+class RadioButtons(QGroupBox):
     """A QGroupBox class for the code execution engine settings.
 
     Settings will allow the user to choose which engine to use when executing
@@ -233,19 +236,21 @@ class CodeEngineSettings(QGroupBox):
         * Script Editor: Qt scriptEditor widget.
     """
 
-    def __init__(self):
+    def __init__(self, title, buttons=None):
         """Initialize the CodeEngineSettings class."""
-        QGroupBox.__init__(self, 'Code Execution Engine')
+        QGroupBox.__init__(self, title)
 
         _layout = QVBoxLayout()
         _layout.setAlignment(Qt.AlignCenter)
-        _layout.addWidget(self.setup_button('Nuke Internal', True))
-        _layout.addWidget(self.setup_button('Script Editor', False))
+
+        if buttons:
+            for button in buttons:
+                _layout.addWidget(self.setup_button(
+                    button.label, button.state))
 
         self.setLayout(_layout)
 
-    @staticmethod
-    def setup_button(label, default_state):  # type: (str, bool) -> QRadioButton
+    def setup_button(self, label, default_state):  # type: (str, bool) -> QRadioButton
         """Set a QRadioButton widget.
 
         Set up a QRadioButton widget default state and connects its toggle
@@ -260,7 +265,8 @@ class CodeEngineSettings(QGroupBox):
             _type_: A QRadioButton object.
         """
         settings = AppSettings()
-        setting_name = 'engine/%s' % _format_name(label)
+        setting_name = '%s/%s' % (_format_name(self.title()),
+                                  _format_name(label))
 
         radiobutton = QRadioButton(label)
         radiobutton.setChecked(settings.get_bool(setting_name, default_state))
@@ -281,7 +287,12 @@ class SettingsWidget(QWidget):
         self.setObjectName('SettingsWidget')
 
         _layout = QVBoxLayout()
-        _layout.addWidget(CodeEngineSettings())
+        _layout.addWidget(RadioButtons('Code Execution Engine',
+                                       [_RadioButton('Nuke Internal', True),
+                                        _RadioButton('Script Editor', False)]))
+        _layout.addWidget(RadioButtons('Connection Type',
+                                       [_RadioButton('TCP', True),
+                                        _RadioButton('WebSocket', False)]))
         _layout.addWidget(ScriptEditorSettings())
         _layout.addWidget(TimeoutSettings())
 
