bccf1cc02d4e2d20ea665c9e6010bc00a8218724
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-01-28 15:02:18 -0500

    Refactor logger

--- src/logger.py
+++ src/logger.py
@@ -1,63 +1,69 @@
 """Logging module."""
 # coding: utf-8
-from __future__ import print_function
 
 import os
 import sys
 import logging
 
-LOG_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'log')
-
-if not os.path.exists(LOG_PATH):
-    os.mkdir(LOG_PATH)
-
-LOGGER = logging.getLogger('NukeServerSocket')
+LOGGER = logging.getLogger('nukeserversocket')
 LOGGER.propagate = False
 LOGGER.setLevel(logging.DEBUG)
 
-DEBUG_FILE = os.path.join(LOG_PATH, 'debug.log')
 
-BASE_FORMAT = logging.Formatter(
-    '[%(asctime)s]  %(levelname)-10s %(filename)-25s %(funcName)-25s :: %(message)s',
-    "%m-%d %I:%M%p")
+def _formatter_detailed():
+    return logging.Formatter(
+        '%(levelname)s | %(asctime)s | %(name)s | '
+        '%(filename)s.%(funcName)s:%(lineno)s | %(message)s'
+    )
 
 
-def empty_line():
+def _formatter_console():
+    return logging.Formatter(
+        '%(levelname)-10s - %(module)s:%(lineno)-5s %(funcName)-15s :: %(message)s'
+    )
+
+
+def _get_path(name):
+    logs_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logs')
+    if not os.path.exists(logs_path):
+        os.mkdir(logs_path)
+    return os.path.join(logs_path, f'{name}.log')
+
+
+def empty_line(log_file='debug'):
     """Append an empty line in the debug.log fie."""
-    with open(DEBUG_FILE, 'a+') as file:
+    with open(_get_path(log_file), 'a+') as file:
         file.write('\n')
 
 
 def set_critical():
     """Init function for the critical handler logger."""
-    critical = logging.FileHandler(os.path.join(LOG_PATH, 'errors.log'), 'w')
+    critical = logging.FileHandler(_get_path('error'), 'w')
     critical.setLevel(logging.ERROR)
-    critical.setFormatter(BASE_FORMAT)
-    critical.set_name('Critical')
+    critical.setFormatter(_formatter_detailed())
+    critical.set_name('critical')
     return critical
 
 
 def set_debug():
     """Init function for the debug handler logger."""
     empty_line()
-    debug = logging.FileHandler(DEBUG_FILE, 'w')
-    debug.set_name('Debug')
+    debug = logging.FileHandler(_get_path('debug'), 'w')
+    debug.set_name('debug')
     debug.setLevel(logging.DEBUG)
-    debug.setFormatter(BASE_FORMAT)
+    debug.setFormatter(_formatter_detailed())
     return debug
 
 
 def set_console():
     """Init function for the console handler logger."""
-    console_format = logging.Formatter(
-        '%(module)s:%(lineno)-5s %(funcName)-15s :: %(message)s')
     console = logging.StreamHandler(stream=sys.stdout)
-    console.set_name('Console')
+    console.set_name('console')
     console.setLevel(logging.INFO)
-    console.setFormatter(console_format)
+    console.setFormatter(_formatter_console())
     return console
 
 
-LOGGER.addHandler(set_console())
-LOGGER.addHandler(set_critical())
 LOGGER.addHandler(set_debug())
+LOGGER.addHandler(set_critical())
+LOGGER.addHandler(set_console())
