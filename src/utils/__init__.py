07b21aef67eda80c01650132071fa052c5051f61
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-01-28 10:44:01 -0500

    remove package

--- src/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Some utility functions and the AppSettings class."""
-from .util import (
-    validate_output, get_ip, insert_time,
-    pyDecoder, pyEncoder
-)
-from .settings import AppSettings
