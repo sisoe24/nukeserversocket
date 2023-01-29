a07117ed9cfe3828c84a12b58e6e48a42f1ced05
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-01-29 12:27:08 -0500

    Update tests

--- tests/network/test_connection_classes.py
+++ tests/network/test_connection_classes.py
@@ -1,21 +1,21 @@
 """Test the CodeEditor class."""
-import pytest
 from PySide2.QtNetwork import QTcpServer, QTcpSocket
 from PySide2.QtWebSockets import QWebSocket, QWebSocketServer
 
 from src.settings import AppSettings
 from src.network.nss_client import QBaseClient
 from src.network.nss_server import QServer
+from src.network.nss_socket import _TcpSocket, _WebSocket
 
 
-def test_server_is_websocket():
+def test_server_is_websocket_server():
     """If websocket settings is true, server should be a webserversocket."""
     AppSettings().setValue('connection_type/websocket', True)
     server = QServer().server
     assert isinstance(server, QWebSocketServer)
 
 
-def test_server_is_tcp():
+def test_server_is_tcp_server():
     """If websocket settings is false, server should be a tcpserver."""
     server = QServer().server
     assert isinstance(server, QTcpServer)
@@ -24,11 +24,26 @@ def test_server_is_tcp():
 def test_test_client_is_websocket():
     """If websocket settings is true, client should be a websocket."""
     AppSettings().setValue('connection_type/websocket', True)
-    socket = QBaseClient('localhost', 54321).socket.socket
+    socket = QBaseClient('localhost', 54321).socket._socket
     assert isinstance(socket, QWebSocket)
 
 
 def test_test_client_is_tcpsocket():
     """If websocket settings is false, socket should be a tcpsocket."""
-    socket = QBaseClient('localhost', 54321).socket.socket
+    AppSettings().setValue('connection_type/tcp', True)
+    socket = QBaseClient('localhost', 54321).socket._socket
     assert isinstance(socket, QTcpSocket)
+
+
+def test_abstract_tcp_socket():
+    """Test if abstract socket class is _TcpSocket."""
+    AppSettings().setValue('connection_type/tcp', True)
+    socket = QBaseClient('localhost', 54321).socket
+    assert isinstance(socket, _TcpSocket)
+
+
+def test_abstract_websocket():
+    """Test if abstract socket class is _WebSocket."""
+    AppSettings().setValue('connection_type/websocket', True)
+    socket = QBaseClient('localhost', 54321).socket
+    assert isinstance(socket, _WebSocket)
