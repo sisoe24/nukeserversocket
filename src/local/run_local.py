3e11788f70cdab08afc3fd7444e9a36cda2b377d
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-01-29 13:54:01 -0500

    Refactor local module

--- src/local/run_local.py
+++ src/local/run_local.py
@@ -1,55 +1,19 @@
 """Run application locally outside Nuke."""
 # coding: utf-8
-from __future__ import print_function
-
+import os
 import sys
 import logging
-
 from random import randint
 
-from PySide2.QtWidgets import (
-    QApplication,
-    QPushButton,
-    QVBoxLayout,
-    QWidget,
-)
+from PySide2.QtWidgets import QWidget, QVBoxLayout, QApplication
 
+from ..main import MainWindow, MainWindowWidget
 from ..widgets import FakeScriptEditor
-from ..main import MainWindowWidget, MainWindow
+
+LOGGER = logging.getLogger('nukeserversocket')
 
 
-LOGGER = logging.getLogger('NukeServerSocket.runapp')
-
-
-class SecondFakeScriptEditor(QWidget):
-    """Second fake script editor class.
-
-    I wanted to test what happens when a second script editor gets created, and
-    the other gets deleted.
-    """
-
-    def __init__(self, se1):
-        """Init method for the SecondFakeScriptEditor class."""
-        QWidget.__init__(self)
-        self.se1 = se1
-        self.se2 = FakeScriptEditor('uk.co.thefoundry.scripteditor.2')
-
-        delete_btn = QPushButton('Delete Editor')
-        delete_btn.clicked.connect(self.delete_widget)
-
-        _layout = QVBoxLayout()
-        _layout.addWidget(delete_btn)
-        _layout.addWidget(self.se2)
-
-        self.setLayout(_layout)
-
-    def delete_widget(self):
-        """Delete the script editor1."""
-        print('delete editor.1')
-        self.se1.deleteLater()
-
-
-class _MainWindowWidget(QWidget):
+class NukeServerSocketLocal(QWidget):
     """Main window widget that holds the main app and the fake script editor."""
 
     def __init__(self):
@@ -84,29 +48,14 @@ class _MainWindow(MainWindow):
 
     def __init__(self):
         """Init method for the _MainWindow class."""
-        MainWindow.__init__(self, main_widget=_MainWindowWidget)
-        self.setWindowTitle('NukeServerSocket Test')
-
-        # personal monitor loc for rapid testing
-        screen_loc = {
-            'hp': {'x': -1077.296875, 'y': -5.31640625}
-        }
-        self.setGeometry(screen_loc['hp']['x'],
-                         screen_loc['hp']['y'],
-                         1080, 1980)
-
-
-class MyApplication(QApplication):
-    """Custom QApplication class."""
-
-    def __init__(self, *args):
-        """Init method for the MyApplication class."""
-        super(MyApplication, self).__init__(*args)
-        self.window = _MainWindow()
-        self.window.show()
+        MainWindow.__init__(self, main_widget=NukeServerSocketLocal)
+        self.setWindowTitle('NukeServerSocket Local')
+        if os.getenv('USERNAME', os.getenv('USER')) == 'virgil':
+            self.setGeometry(1449.26171875, 602.8671875, 1080, 1980)
 
 
 if __name__ == '__main__':
-
-    app = MyApplication(sys.argv)
+    app = QApplication(sys.argv)
+    window = _MainWindow()
+    window.show()
     app.exec_()
