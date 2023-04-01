eeb4318c931a5aaed63adcad0e8fa802b28f05ce
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-04-01 00:03:15 -0400

    Update tests

--- tests/controllers/test_code_editor.py
+++ tests/controllers/test_code_editor.py
@@ -7,11 +7,6 @@ from nukeserversocket.network import DataCode
 from nukeserversocket.controllers import nuke_controllers as se
 
 
-def _data_code(code='test', file=''):
-    """Return a initialized DataCode object."""
-    return DataCode(json.dumps({'text': code, 'file': file}))
-
-
 @pytest.mark.usefixtures('_init_fake_editor')
 class TestCodeEditor:
     """Test the Code Editor class initialization.
@@ -22,24 +17,24 @@ class TestCodeEditor:
     @pytest.mark.parametrize('file', ['file.py', ''])
     def test_is_python_controller(self, file):
         """Check if is a valid PyController when file ends .py, None."""
-        controller = se.controllers_factory(_data_code(file=file))
+        controller = se.get_controllers(file)
         assert isinstance(controller, se._PyController)
 
     @pytest.mark.parametrize('file', ['file.cpp', 'file.blink'])
     def test_is_blink_controller(self, file):
         """Check if is a valid BlinkController when file ends .cpp, .blink."""
-        controller = se.controllers_factory(_data_code(file=file))
+        controller = se.get_controllers(file)
         assert isinstance(controller, se._BlinkController)
 
     def test_is_nodes_controller(self):
         """Check if is a valid CopyNodesController when file ends .tmp."""
-        controller = se.controllers_factory(_data_code(file='file.tmp'))
+        controller = se.get_controllers(file='file.tmp')
         assert isinstance(controller, se._CopyNodesController)
 
     def test_execute_script_editor_code(self):
         """Test executing code from the script editor instead of nuke internal."""
         code = 'print("hello".upper())'
-        controller = se.controllers_factory(_data_code(code, file='tmp.py'))
+        controller = se.get_controllers(file='tmp.py')
 
         # when bypassing the main execute function, need to set the input
         controller.set_input(code)
