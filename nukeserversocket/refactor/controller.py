69833a159605a9d2904db919a01234c57eaa9245
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-16 13:40:45 -0500

    Refactor controller.py: Add logging and history tracking

--- nukeserversocket/refactor/controller.py
+++ nukeserversocket/refactor/controller.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 import os
+import logging
 from abc import ABC, abstractmethod
-from typing import Callable, Optional
+from typing import List, Callable, Optional
 from datetime import datetime
 
 from PySide2.QtWidgets import QTextEdit, QPlainTextEdit
@@ -10,13 +11,16 @@ from PySide2.QtWidgets import QTextEdit, QPlainTextEdit
 from .settings import get_settings
 from .received_data import ReceivedData
 
+logging.basicConfig(level=logging.DEBUG)
+
 
 def format_output(data: ReceivedData, result: str) -> str:
     placeholders = {
         '%d': datetime.now().strftime('%H:%M:%S'),
         '%f': data.file,
         '%F': os.path.basename(data.file),
-        '%t': data.text,
+        '%t': result,
+        '%n': '\n'
     }
 
     output_format = get_settings().get('format_output')
@@ -24,10 +28,11 @@ def format_output(data: ReceivedData, result: str) -> str:
         if key in output_format:
             output_format = output_format.replace(key, value)
 
-    return f'{output_format}\n{result}'
+    return output_format
 
 
 class BaseEditorController(ABC):
+    history: List[str] = []
 
     @property
     @abstractmethod
@@ -40,6 +45,43 @@ class BaseEditorController(ABC):
     @abstractmethod
     def execute(self) -> None: ...
 
+    @abstractmethod
+    def get_output(self) -> str: ...
+
+    @classmethod
+    def _add_to_history(cls, text: str) -> None:
+        cls.history.append(text)
+
+    def _process_output(self, data: ReceivedData, result: str) -> str:
+
+        settings = get_settings()
+
+        if settings.get('format_output'):
+            logging.debug('formatting output')
+            output = format_output(data, result)
+        else:
+            logging.debug('not formatting output')
+            output = result
+
+        logging.debug(f'final output: -> {output} <-')
+
+        self._add_to_history(output)
+
+        if settings.get('clear_output'):
+            self.output_editor.setPlainText(output)
+        else:
+            # TODO: Investigate why insertPlainText or appendPlainText
+            # does not work as expected
+            self.output_editor.setPlainText(
+                '\n'.join(self.history) + '\n'
+            )
+
+        self.output_editor.verticalScrollBar().setValue(
+            self.output_editor.verticalScrollBar().maximum()
+        )
+
+        return output
+
     def run(self, data: ReceivedData) -> str:
         initial_input = self.input_editor.toPlainText()
         initial_output = self.output_editor.toPlainText()
@@ -48,27 +90,15 @@ class BaseEditorController(ABC):
 
         self.execute()
 
-        result = self.output_editor.toPlainText()
+        result = self.get_output()
+        logging.debug(f'result: {result}')
 
-        settings = get_settings()
-        print(settings)
-
-        if settings.get('mirror_script_editor'):
-            self.input_editor.setPlainText(data.text)
-            self.output_editor.setPlainText(result)
+        if not get_settings().get('mirror_script_editor'):
+            self.input_editor.setPlainText(initial_input)
+            self.output_editor.setPlainText(initial_output)
             return result
 
-        # if settings.get('clear_output', True):
-        #     self.output_editor.setPlainText('')
-
-        if settings.get('format_output'):
-            self.output_editor.setPlainText(format_output(data, result))
-
-        # else:
-        #     self.input_editor.setPlainText(initial_input)
-        #     self.output_editor.setPlainText(initial_output)
-
-        return result
+        return self._process_output(data, result)
 
 
 C = Callable[[], BaseEditorController]
