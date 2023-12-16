8dd94b8bdf570c3ad2e9754571d5305779f08321
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-16 14:19:57 -0500

    Refactor controller.py: Limit history size and set input from ReceivedData

--- nukeserversocket/refactor/controller.py
+++ nukeserversocket/refactor/controller.py
@@ -45,15 +45,12 @@ class BaseEditorController(ABC):
     @abstractmethod
     def execute(self) -> None: ...
 
-    def get_output(self) -> str:
-        return self.output_editor.toPlainText()
-
     @classmethod
     def _add_to_history(cls, text: str) -> None:
+        # TODO: Limit history size
         cls.history.append(text)
 
     def _process_output(self, data: ReceivedData, result: str) -> str:
-
         settings = get_settings()
 
         if settings.get('format_output'):
@@ -72,9 +69,7 @@ class BaseEditorController(ABC):
         else:
             # TODO: Investigate why insertPlainText or appendPlainText
             # does not work as expected
-            self.output_editor.setPlainText(
-                '\n'.join(self.history) + '\n'
-            )
+            self.output_editor.setPlainText('\n'.join(self.history) + '\n')
 
         self.output_editor.verticalScrollBar().setValue(
             self.output_editor.verticalScrollBar().maximum()
@@ -82,16 +77,20 @@ class BaseEditorController(ABC):
 
         return output
 
+    def get_output(self) -> str:
+        return self.output_editor.toPlainText()
+
+    def set_input(self, data: ReceivedData) -> None:
+        logging.debug(f'base class file: {data.file}')
+        self.input_editor.setPlainText(data.text)
+
     def run(self, data: ReceivedData) -> str:
         initial_input = self.input_editor.toPlainText()
         initial_output = self.output_editor.toPlainText()
 
-        self.input_editor.setPlainText(data.text)
-
+        self.set_input(data)
         self.execute()
-
         result = self.get_output()
-        logging.debug(f'result: {result}')
 
         if not get_settings().get('mirror_script_editor'):
             self.input_editor.setPlainText(initial_input)
