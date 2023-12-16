ecec035e6ff61949a3ca5ed5bcc4ff7dd31cff13
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-16 10:44:08 -0500

    Refactor NssServer class and add Controller dependency

--- nukeserversocket/refactor/server.py
+++ nukeserversocket/refactor/server.py
@@ -1,54 +1,16 @@
 from __future__ import annotations
 
 import os
-import json
 from typing import Optional
-from dataclasses import field, dataclass
 
 from PySide2.QtCore import Slot, Signal, QObject
 from PySide2.QtNetwork import QTcpServer, QTcpSocket, QHostAddress
 
-from .settings import get_settings
+from .controller import Controller
+from .received_data import ReceivedData
 
 
-def server_factory() -> QTcpServer:
-    """Factory function to create a new server object.
-
-    Returns:
-        NssServer: NssServer object.
-    """
-    if os.getenv('NUKE_SERVER_SOCKET_CONNECTION_TYPE', 'tcp') == 'websocket':
-        raise NotImplementedError('Websocket not implemented yet.')
-    return QTcpServer()
-
-
-@dataclass
-class ReceivedData:
-    data: str
-    file: str = field(init=False)
-    text: str = field(init=False)
-
-    def __post_init__(self):
-
-        try:
-            d = json.loads(self.data)
-        except json.JSONDecodeError as e:
-            raise ValueError('Data is not a valid json.') from e
-
-        self.text = d.get('text')
-        if not self.text:
-            raise ValueError('Data does not contain a text field.')
-
-        # file is optional as is only used to show the file name in the output
-        self.file = d.get('file')
-
-    def output(self):
-        if get_settings().get('format_output', False):
-            return f'{self.file}\nText: {self.text}'.strip()
-        return self.text
-
-
-class NssServer(QObject):
+class NssServer(QTcpServer):
     on_new_connection = Signal()
     on_data_received = Signal(str)
     on_data_written = Signal(str)
@@ -56,17 +18,24 @@ class NssServer(QObject):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.server = server_factory()
-        self.server.newConnection.connect(self._on_new_connection)
+        self.newConnection.connect(self._on_new_connection)
 
         self._socket: Optional[QTcpSocket] = None
 
+        controller = Controller.get_instance()
+        if not controller:
+            raise RuntimeError('Controller is not set.')
+        self._controller = controller()
+
     @Slot()
     def _on_socket_ready(self):
+        if not self._socket:
+            raise RuntimeError('Socket is not set.')
 
         data = ReceivedData(self._socket.readAll().data())
 
-        output = data.output()
+        output = self._controller.execute(data)
+
         self.on_data_received.emit(data.text)
         self.on_data_written.emit(output)
 
@@ -74,19 +43,13 @@ class NssServer(QObject):
         self._socket.close()
 
     def _on_new_connection(self) -> None:
-        while self.server.hasPendingConnections():
-            self._socket = self.server.nextPendingConnection()
+        while self.hasPendingConnections():
+            self._socket = self.nextPendingConnection()
 
             self._socket.readyRead.connect(self._on_socket_ready)
-            self._socket.connected.connect(lambda: print('connected'))
-            self._socket.disconnected.connect(lambda: print('disconnected'))
-            self._socket.error.connect(lambda err: print('error', err))
+            # self._socket.connected.connect(lambda: print('connected'))
+            # self._socket.disconnected.connect(lambda: print('disconnected'))
+            # self._socket.error.connect(lambda err: print('error', err))
 
-    def is_listening(self) -> bool:
-        return self.server.isListening()
-
-    def close(self) -> None:
-        self.server.close()
-
-    def listen(self, port: int) -> bool:
-        return bool(self.server.listen(QHostAddress.Any, port))
+    def can_connect(self, port: int) -> bool:
+        return bool(self.listen(QHostAddress.Any, port))
