ca32637369ef0c52526b802d5df0a9bd60aabe8e
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-16 19:37:10 -0500

    Add documentation for ReceivedData class

--- nukeserversocket/received_data.py
+++ nukeserversocket/received_data.py
@@ -6,7 +6,21 @@ from dataclasses import field, dataclass
 
 @dataclass
 class ReceivedData:
-    """Data received from the client."""
+    """Data received from the client.
+
+    Data is expected to be a json string with the following format:
+
+    {
+        "text": "Text to run in the script editor",
+        "file": "File name to show in the output"
+    }
+
+    NOTE: From version 1.0.0, the `file` field is mandatory but it can be an empty string.
+
+    Raises:
+        ValueError: If the data is not a valid json string or if it does not contain a `text` field.
+
+    """
 
     data: str
     file: str = field(init=False)
