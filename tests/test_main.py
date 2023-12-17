a30dc5f83201c8f204874d03fc00b8393616a1aa
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-16 19:48:15 -0500

    Add test for NukeServerSocket class

---
+++ tests/test_main.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from nukeserversocket.main import NukeServerSocket
+
+
+def test_nuke_server_socket():
+    """Test the NukeServerSocket class."""
+    assert NukeServerSocket
