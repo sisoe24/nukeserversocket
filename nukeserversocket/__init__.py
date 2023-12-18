f398fc458d2600c3813877d7d12c8ec99a73137f
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-17 22:53:37 -0500

    Update NukeServerSocket controllers

--- nukeserversocket/__init__.py
+++ nukeserversocket/__init__.py
@@ -1,3 +1,3 @@
 """Module will initialize the logging system and import Nuke."""
 from .main import NukeServerSocket
-from .plugins.nuke import install_nuke
+from .controllers.nuke import install_nuke
