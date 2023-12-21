f35fe322e3243ff3b7a60b35e77e90bae6ac0262
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-20 22:10:11 -0500

    Delete test_main.py

--- tests/test_main.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from nukeserversocket.main import NukeServerSocket
-
-
-def test_nuke_server_socket():
-    """Test the NukeServerSocket class."""
-    assert NukeServerSocket
