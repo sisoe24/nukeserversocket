0bcf627a6033bb22b50a724174d8c081b4f8ff41
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-20 22:09:59 -0500

    Refactor ReceivedData class to handle raw data and add type hinting

--- nukeserversocket/received_data.py
+++ nukeserversocket/received_data.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import json
+from typing import Dict
 from dataclasses import field, dataclass
 
 
@@ -22,20 +23,22 @@ class ReceivedData:
 
     """
 
-    data: str
+    raw: str
+    data: Dict[str, str] = field(init=False)
     file: str = field(init=False)
     text: str = field(init=False)
 
     def __post_init__(self):
 
         try:
-            d = json.loads(self.data)
-        except json.JSONDecodeError as e:
-            raise ValueError('Data is not a valid json.') from e
+            self.data = json.loads(self.raw)
+            self.data.setdefault('file', '')
+        except Exception as e:
+            raise ValueError(f'An exception occurred while decoding the data. {e}') from e
 
-        self.text = d.get('text')
+        self.text = self.data.get('text')
         if not self.text:
             raise ValueError('Data does not contain a text field.')
 
         # file is optional as is only used to show the file name in the output
-        self.file = d.get('file')
+        self.file = self.data.get('file')
