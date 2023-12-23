abdb58bf874ad2f9f5b89537ab7f78be0d4795c6
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-23 12:37:01 -0500

    Refactor test_get_settings function in test_settings.py

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -5,12 +5,18 @@ import pathlib
 from nukeserversocket.settings import _NssSettings, get_settings
 
 
-def test_get_settings(tmp_settings: pathlib.Path):
+def test_get_settings_default(tmp_settings: pathlib.Path):
     settings = get_settings()
+
     assert isinstance(settings, _NssSettings)
     assert settings.data == _NssSettings.defaults
 
+
+def test_get_settings(tmp_settings: pathlib.Path):
+    settings = get_settings()
+
     assert settings.get('temp_value') is None
+
     settings.set('temp_value', True)
     assert settings.get('temp_value') is True
 
