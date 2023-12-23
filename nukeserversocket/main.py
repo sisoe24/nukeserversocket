1b3598ff048ffc7ddc8c6d12a9bf8009615daa90
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-23 10:56:00 -0500

    Refactor main.py: Update UI layout and logging

--- nukeserversocket/main.py
+++ nukeserversocket/main.py
@@ -2,22 +2,21 @@
 from __future__ import annotations
 
 import socket
-import logging
-from datetime import datetime
 
-from PySide2.QtCore import Slot, QTimer
+from PySide2.QtCore import Qt, Slot, QTimer
 from PySide2.QtWidgets import (QLabel, QWidget, QSpinBox, QFormLayout,
-                               QMainWindow, QPushButton, QVBoxLayout,
-                               QPlainTextEdit)
+                               QMainWindow, QPushButton, QVBoxLayout)
 
 from nukeserversocket.editor_controller import EditorController
 
 from .about import about
+from .logger import get_logger
 from .server import NssServer
+from .console import NssConsole
 from .toolbar import ToolBar
 from .settings import _NssSettings, get_settings
 
-logging.basicConfig(level=logging.DEBUG)
+LOGGER = get_logger()
 
 
 class MainModel:
@@ -54,10 +53,7 @@ class MainView(QWidget):
         self.connect_btn = QPushButton('Connect')
         self.connect_btn.setCheckable(True)
 
-        self.logs = QPlainTextEdit()
-        self.logs.setReadOnly(True)
-
-        self.clear_logs = QPushButton('Clear logs')
+        self.console = NssConsole()
 
         self.status_label = QLabel('Idle')
         self.set_disconnected()
@@ -68,6 +64,7 @@ class MainView(QWidget):
         self.port_input.setRange(49512, 65535)
 
         form_layout = QFormLayout()
+        form_layout.setAlignment(Qt.AlignVCenter)
         form_layout.addRow('Status:', self.status_label)
         form_layout.addRow('IP:', self.ip_label)
         form_layout.addRow('Port:', self.port_input)
@@ -75,9 +72,7 @@ class MainView(QWidget):
         layout = QVBoxLayout()
         layout.addLayout(form_layout)
         layout.addWidget(self.connect_btn)
-        layout.addWidget(QLabel('Logs'))
-        layout.addWidget(self.logs)
-        layout.addWidget(self.clear_logs)
+        layout.addWidget(self.console)
 
         self.setLayout(layout)
 
@@ -101,37 +96,24 @@ class MainController:
 
         self._server = server
         self._server.on_data_received.connect(self._on_data_received)
-        self._server.on_data_written.connect(self._on_data_written)
 
         self._view.ip_label.setText(self._model.get_ip())
         self._view.port_input.setValue(self._model.get_port())
         self._view.port_input.valueChanged.connect(self._on_port_change)
         self._view.connect_btn.clicked.connect(self._on_connect)
-        self._view.clear_logs.clicked.connect(self._view.logs.clear)
 
         self._timer = QTimer()
         self._timer.timeout.connect(self._on_timeout)
         self._timer.setSingleShot(True)
 
-    def _write_log(self, text: str):
-        time = datetime.now().strftime('%H:%M:%S')
-        self._view.logs.appendPlainText(f'[{time}] {text}')
-        self._view.logs.verticalScrollBar().setValue(
-            self._view.logs.verticalScrollBar().maximum())
-
     @Slot()
     def _on_timeout(self):
-        self._write_log('Server Timeout.')
+        LOGGER.debug('Server Timeout.')
         self._close_connection()
 
     @Slot(str)
-    def _on_data_received(self, data: str):
-        self._write_log(f'Received data:\n {data}')
+    def _on_data_received(self):
         self._timer.start(self._model.get_server_timeout())
-
-    @Slot(str)
-    def _on_data_written(self, data: str):
-        self._write_log(f'Data written:\n {data}')
 
     @Slot(int)
     def _on_port_change(self):
@@ -142,12 +124,14 @@ class MainController:
         port = self._view.port_input.value()
         if should_connect and not self._server.isListening():
             if self._server.try_connect(port):
+                LOGGER.info('Listening on %s...', port)
+
                 self._timer.start(self._model.get_server_timeout())
-                self._write_log(f'Listening on {port}...')
                 self._view.port_input.setEnabled(False)
                 self._view.set_connected()
+
             else:
-                self._write_log(f'Failed to establish connection on port {port}.')
+                LOGGER.error('Failed to establish connection. %s.', self._server.errorString())
                 self._close_connection()
         else:
             self._close_connection()
@@ -157,7 +141,7 @@ class MainController:
         self._view.set_disconnected()
         self._view.port_input.setEnabled(True)
         self._view.connect_btn.setChecked(False)
-        self._write_log('Closing connection...')
+        LOGGER.info('Closing connection...')
 
 
 class NukeServerSocket(QMainWindow):
