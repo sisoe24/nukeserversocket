898e28171caef40da6bc1acf2749aeadc85ab123
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-24 10:33:50 -0500

    Fix import statement in __init__.py

--- nukeserversocket/__init__.py
+++ nukeserversocket/__init__.py
@@ -2,5 +2,5 @@
 from __future__ import annotations
 
 from .main import NukeServerSocket
-from .controllers.nuke import install_nuke
 from .version import __version__
+from .controllers.nuke import install_nuke
