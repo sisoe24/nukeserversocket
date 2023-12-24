3490f094e8fd3a158389f80f0ef8dff886c881ec
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-23 22:25:23 -0500

    Refactor logging in server.py

--- nukeserversocket/server.py
+++ nukeserversocket/server.py
@@ -1,15 +1,15 @@
 from __future__ import annotations
 
+import logging
 from typing import Optional
 
 from PySide2.QtCore import Slot, Signal
 from PySide2.QtNetwork import QTcpServer, QTcpSocket, QHostAddress
 
-from .logger import get_logger
 from .received_data import ReceivedData
 from .editor_controller import EditorController
 
-LOGGER = get_logger()
+LOGGER = logging.getLogger('nukeserversocket')
 
 
 class NssServer(QTcpServer):
