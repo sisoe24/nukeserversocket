ee2d0c4536c49dcf7bb12e1c459f4c758ad20990
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-24 12:48:37 -0500

    Refactor test fixtures and add logging configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,37 @@
 from __future__ import annotations
 
 import os
+import logging
 import pathlib
 
 import pytest
 
+from nukeserversocket.settings import _NssSettings
 from nukeserversocket.utils.cache import clear_cache
 from nukeserversocket.controllers.local_app import LocalEditor
 
+SETTINGS_FILE = pathlib.Path(__file__).parent / 'tmp' / 'nss.json'
+SETTINGS_FILE.parent.mkdir(parents=True, exist_ok=True)
+
+
+@pytest.fixture(autouse=True)
+def editor():
+    LOGGER = logging.getLogger('nukeserversocket')
+    LOGGER.propagate = False
+    LOGGER.handlers.clear()
+
+
+@pytest.fixture(autouse=True)
+def mock_settings():
+    SETTINGS_FILE.write_text('{}')
+
+    os.environ['NSS_SETTINGS'] = str(SETTINGS_FILE)
+    yield SETTINGS_FILE
+
+    clear_cache('settings')
+    SETTINGS_FILE.write_text('{}')
+
 
 @pytest.fixture()
-def tmp_settings():
-    f = pathlib.Path(__file__).parent / 'tmp' / 'nss.json'
-    f.parent.mkdir(parents=True, exist_ok=True)
-    f.write_text('{}')
-
-    os.environ['NSS_SETTINGS'] = str(f)
-
-    yield f
-    clear_cache('settings')
-
-    f.unlink()
+def settings(mock_settings: pathlib.Path):
+    return _NssSettings(mock_settings)
