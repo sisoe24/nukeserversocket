b6bffe32122328ee1bfce6d54628544947b7cb99
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-23 22:29:05 -0500

    Add type hinting to __init__.py and update cache import in utils/__init__.py

--- nukeserversocket/__init__.py
+++ nukeserversocket/__init__.py
@@ -1,3 +1,5 @@
 """Module will initialize the logging system and import Nuke."""
+from __future__ import annotations
+
 from .main import NukeServerSocket
 from .controllers.nuke import install_nuke
--- nukeserversocket/utils/__init__.py
+++ nukeserversocket/utils/__init__.py
@@ -1,2 +1,2 @@
-from .cache import cache
+from .cache import cache, get_cache, clear_cache
 from .get_root import _get_root, ROOT
