20c43f1ff6fda46fc3401b9c491b03c98a6fdc85
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-24 10:34:00 -0500

    Refactor imports and add type hints

--- nukeserversocket/main.py
+++ nukeserversocket/main.py
@@ -2,20 +2,24 @@
 from __future__ import annotations
 
 import socket
-import logging
+from typing import TYPE_CHECKING
 
 from PySide2.QtCore import Qt, Slot, QTimer
 from PySide2.QtWidgets import (QLabel, QWidget, QSpinBox, QFormLayout,
                                QMainWindow, QPushButton, QVBoxLayout)
 
+from .logger import get_logger
 from .server import NssServer
 from .console import NssConsole
 from .toolbar import ToolBar
 from .version import __version__
-from .settings import _NssSettings, get_settings
-from .editor_controller import EditorController
+from .settings import get_settings
 
-LOGGER = logging.getLogger('nukeserversocket')
+if TYPE_CHECKING:
+    from .settings import _NssSettings
+    from .editor_controller import EditorController
+
+LOGGER = get_logger()
 
 
 class MainModel:
