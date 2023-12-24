7fce016cdbed5467d926c85931bce7b5fdf4f4fb
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-24 17:04:22 -0500

    Refactor LocalController in local_app.py

--- nukeserversocket/controllers/local_app.py
+++ nukeserversocket/controllers/local_app.py
@@ -5,29 +5,14 @@ Only used for testing purposes.
 """
 from __future__ import annotations
 
-import io
 import sys
-import contextlib
 
-from PySide2.QtWidgets import (QLabel, QTextEdit, QPushButton, QVBoxLayout,
-                               QApplication, QPlainTextEdit)
+from PySide2.QtWidgets import (QLabel, QTextEdit, QPushButton, QApplication,
+                               QPlainTextEdit)
 
 from ..main import NukeServerSocket
+from ..utils import stdoutIO
 from ..editor_controller import EditorController
-
-
-@contextlib.contextmanager
-def _stdoutIO(stdout=None):
-    """Get output from sys.stdout after executing code from `exec`.
-
-    https://stackoverflow.com/a/3906390/9392852
-    """
-    old = sys.stdout
-    if stdout is None:
-        stdout = io.StringIO()
-    sys.stdout = stdout
-    yield stdout
-    sys.stdout = old
 
 
 class LocalController(EditorController):
@@ -47,7 +32,7 @@ class LocalController(EditorController):
         return self._output_editor
 
     def execute(self) -> None:
-        with _stdoutIO() as s:
+        with stdoutIO() as s:
             exec(self.input_editor.toPlainText())
             result = s.getvalue()
             self.output_editor.setPlainText(result)
