56b74dbc4508cc15bf944d82086e75f267812d02
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-24 18:09:18 -0500

    Add unit tests for NukeController

---
+++ tests/test_nuke.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+import json
+from textwrap import dedent
+
+import pytest
+from PySide2.QtWidgets import QTextEdit, QPushButton, QPlainTextEdit
+
+from nukeserversocket.settings import _NssSettings
+from nukeserversocket.received_data import ReceivedData
+from nukeserversocket.controllers.nuke import Editor, NukeController
+
+# pytestmark = pytest.mark.quick
+
+
+def test_nuke_python(qtbot, settings):
+    data = ReceivedData('{"file": "test.py", "text": "print(\\"hello world\\")"}')
+    editor = NukeController(
+        Editor(
+            input_editor=QPlainTextEdit(),
+            output_editor=QTextEdit(),
+            run_button=QPushButton()
+        )
+    )
+    editor.settings = settings
+    editor.settings.set('mirror_script_editor', True)
+
+    editor.run(data)
+
+    assert editor.editor.input_editor.toPlainText() == 'print("hello world")'
+
+
+@pytest.mark.parametrize('file, text', (
+    ('test.cpp', 'blinkscript'),
+    ('test.blink', 'blinkscript'),
+))
+def test_nuke_blinkscript(qtbot, settings: _NssSettings, file: str, text: str):
+    d = json.dumps({'file': file, 'text': text})
+    data = ReceivedData(d)
+
+    editor = NukeController(
+        Editor(
+            input_editor=QPlainTextEdit(),
+            output_editor=QTextEdit(),
+            run_button=QPushButton()
+        )
+    )
+    editor.settings = settings
+    editor.settings.set('mirror_script_editor', True)
+
+    editor.run(data)
+
+    assert editor.editor.input_editor.toPlainText() == dedent(f"""
+    node = nuke.toNode("test") or nuke.createNode('BlinkScript', 'name test')
+    knobs = node.knobs()
+    knobs['kernelSourceFile'].setValue('{file}')
+    knobs['kernelSource'].setText("{text}")
+    knobs['recompile'].execute()
+    """)
