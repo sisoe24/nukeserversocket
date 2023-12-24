49d9f44b12b8f54fb59d7519f4759a670fc9deb2
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-24 17:24:31 -0500

    Add ConsoleHandler class to logger.py

--- nukeserversocket/logger.py
+++ nukeserversocket/logger.py
@@ -1,12 +1,32 @@
 from __future__ import annotations
 
 import logging
+from typing import TYPE_CHECKING
 from logging.handlers import TimedRotatingFileHandler
 
 from .utils import ROOT, cache
 
+if TYPE_CHECKING:
+    from .console import NssConsole
+
 PACKAGE_LOG = ROOT / 'logs' / 'nukeserversocket.log'
 PACKAGE_LOG.parent.mkdir(parents=True, exist_ok=True)
+
+
+class ConsoleHandler(logging.Handler):
+    def __init__(self, console: NssConsole) -> None:
+        super().__init__()
+        self.set_name('console')
+        self.setLevel(logging.INFO)
+        self.setFormatter(
+            logging.Formatter(
+                '[%(asctime)s] %(levelname)-8s - %(message)s', '%H:%M:%S'
+            )
+        )
+        self._console = console
+
+    def emit(self, record: logging.LogRecord) -> None:
+        self._console.write(self.format(record) + '\n')
 
 
 def _file_handler() -> TimedRotatingFileHandler:
