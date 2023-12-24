1b09b9bdc6bb3767cf708692a1f85ada8e93cbe3
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-24 12:47:58 -0500

    Refactor test_get_settings_default function to use _NssSettings class
    This commit refactors the test_get_settings_default function in the test_settings.py file to use the _NssSettings class from the nukeserversocket.settings module. The function now asserts that the settings object is an instance of _NssSettings and that its data matches the defaults. Additionally, the test now checks that the 'temp_value' key is set to True in the settings object's data and that it is present in the file specified by the settings object's path.

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,23 +1,13 @@
 from __future__ import annotations
 
-import pathlib
-
-from nukeserversocket.settings import _NssSettings, get_settings
+from nukeserversocket.settings import _NssSettings
 
 
-def test_get_settings_default(tmp_settings: pathlib.Path):
-    settings = get_settings()
-
+def test_get_settings_default(settings: _NssSettings):
     assert isinstance(settings, _NssSettings)
     assert settings.data == _NssSettings.defaults
 
-
-def test_get_settings(tmp_settings: pathlib.Path):
-    settings = get_settings()
-
-    assert settings.get('temp_value') is None
-
     settings.set('temp_value', True)
     assert settings.get('temp_value') is True
-
-    assert '"temp_value": true' in tmp_settings.read_text()
+    assert settings.data['temp_value'] is True
+    assert '"temp_value": true' in settings.path.read_text()
