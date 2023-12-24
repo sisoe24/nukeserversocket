b586df1531d0660015531b4b235720e9608e0a84
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-24 17:23:39 -0500

    Refactor test fixtures and remove unused imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,28 +7,24 @@ import pathlib
 import pytest
 
 from nukeserversocket.settings import _NssSettings
-from nukeserversocket.utils.cache import clear_cache
-from nukeserversocket.controllers.local_app import LocalEditor
 
 SETTINGS_FILE = pathlib.Path(__file__).parent / 'tmp' / 'nss.json'
 SETTINGS_FILE.parent.mkdir(parents=True, exist_ok=True)
 
 
-@pytest.fixture(autouse=True)
-def editor():
-    LOGGER = logging.getLogger('nukeserversocket')
-    LOGGER.propagate = False
-    LOGGER.handlers.clear()
+@pytest.fixture(scope='session', autouse=True)
+def patch_logger():
+    logger = logging.getLogger('nukeserversocket')
+    logger.handlers.clear()
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture()
 def mock_settings():
     SETTINGS_FILE.write_text('{}')
 
     os.environ['NSS_SETTINGS'] = str(SETTINGS_FILE)
     yield SETTINGS_FILE
 
-    clear_cache('settings')
     SETTINGS_FILE.write_text('{}')
 
 
