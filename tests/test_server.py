ac3e65b1ac3e1636ab6ae494da949628c0714da4
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-24 17:05:24 -0500

    Refactor server test and update settings

--- tests/test_server.py
+++ tests/test_server.py
@@ -8,10 +8,10 @@ import pytest
 from pytestqt.qtbot import QtBot
 from PySide2.QtWidgets import QTextEdit, QPlainTextEdit
 
+from nukeserversocket.utils import stdoutIO
 from nukeserversocket.server import NssServer
-from nukeserversocket.settings import get_settings
+from nukeserversocket.settings import _NssSettings
 from nukeserversocket.editor_controller import EditorController
-from nukeserversocket.controllers.local_app import _stdoutIO
 
 PORT = 55559
 
@@ -26,6 +26,8 @@ class MockEditorController(EditorController):
         self._output_editor = QTextEdit()
         self._output_editor.setPlainText('initial output')
 
+        self._settings = None
+
     @property
     def input_editor(self):
         return self._input_editor
@@ -35,21 +37,30 @@ class MockEditorController(EditorController):
         return self._output_editor
 
     def execute(self) -> None:
-        with _stdoutIO() as s:
+        with stdoutIO() as s:
             exec(self.input_editor.toPlainText())
             result = s.getvalue()
             self.output_editor.setPlainText(result)
 
 
 @pytest.fixture()
-def editor():
-    return MockEditorController()
+def editor(settings: _NssSettings):
+    editor = MockEditorController()
+    editor.settings = settings
+
+    yield editor
+
+    editor.history.clear()
+    editor.output_editor.setPlainText('initial output')
+    editor.input_editor.setPlainText('initial input')
 
 
 @pytest.fixture()
 def server(editor: MockEditorController) -> NssServer:
     server = NssServer(editor)
+
     yield server
+
     server.close()
 
 
@@ -64,32 +75,32 @@ def send_data(port: int):
     s.close()
 
 
-def test_server_on_new_connection(qtbot: QtBot, server: NssServer, editor: MockEditorController):
+def test_server_on_new_connection(qtbot: QtBot, server: NssServer):
 
-    settings = get_settings()
-    settings.set('mirror_script_editor', False)
+    server._editor.settings.set('mirror_script_editor', False)
 
     port = 55559
     server.try_connect(port)
 
     send_data(port)
 
-    qtbot.wait(1000)
-    assert editor.output_editor.toPlainText() == 'initial output'
-    assert editor.input_editor.toPlainText() == 'initial input'
+    qtbot.wait(100)
+
+    assert server._editor.output_editor.toPlainText() == 'initial output'
+    assert server._editor.input_editor.toPlainText() == 'initial input'
 
 
 def test_server_on_new_connection_mirror_editor(qtbot: QtBot, server: NssServer):
-    settings = get_settings()
-    settings.set('mirror_script_editor', True)
-    settings.set('format_output', '')
+    server._editor.settings.set('mirror_script_editor', True)
+    server._editor.settings.set('format_output', '')
+    server._editor.settings.set('clear_output', True)
 
     port = 55559
     server.try_connect(port)
 
     send_data(port)
 
-    qtbot.wait(1000)
+    qtbot.wait(100)
 
-    assert server._editor.output_editor.toPlainText() == 'HELLO WORLD\n\n'
+    assert server._editor.output_editor.toPlainText().strip() == 'HELLO WORLD'
     assert server._editor.input_editor.toPlainText() == "print('hello world'.upper())"
