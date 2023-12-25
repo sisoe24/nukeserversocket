d45e9f72abde53e9ce5bf5cf2c94a2db34ca6e23
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-12-25 13:19:56 -0500

    Remove unused import statement

--- nukeserversocket/utils/cache.py
+++ nukeserversocket/utils/cache.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Dict, TypeVar, Callable, Optional
+from typing import Any, Dict, TypeVar, Callable
 
 R = TypeVar('R')
 GenericFunc = Callable[..., R]
