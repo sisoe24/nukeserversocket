826327686333e5c51d8637d1ce15124e050ae779
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2024-08-17 23:44:30 -0400

    Add colors to log level

--- nukeserversocket/console.py
+++ nukeserversocket/console.py
@@ -11,6 +11,14 @@ from .logger import get_logger
 
 LOGGER = get_logger()
 
+LOG_COLORS = {
+    'DEBUG': 'aqua',
+    'INFO': 'white',
+    'WARNING': 'orange',
+    'ERROR': 'red',
+    'CRITICAL': 'magenta',
+}
+
 
 class NssConsole(QGroupBox):
     def __init__(self, parent=None):
@@ -50,8 +58,9 @@ class NssConsole(QGroupBox):
     def _on_enable_debug(self, state: int) -> None:
         LOGGER.console.setLevel(logging.DEBUG if state == 2 else logging.INFO)
 
-    def write(self, text: str) -> None:
-        self._console.insertPlainText(text)
+    def write(self, text: str, level_name: str = 'INFO') -> None:
+        color = LOG_COLORS.get(level_name, 'white')
+        self._console.appendHtml(f'<font color="{color}">{text}</font>')
         self._console.verticalScrollBar().setValue(
             self._console.verticalScrollBar().maximum()
         )
