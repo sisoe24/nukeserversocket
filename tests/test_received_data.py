4661bb36832a0fabb396dfb7dfc0cad8bdff4952
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2024-08-17 23:05:20 -0400

    Update tests for received data

--- tests/test_received_data.py
+++ tests/test_received_data.py
@@ -10,6 +10,7 @@ from nukeserversocket.received_data import ReceivedData
 
 @dataclass
 class ReceivedTestData:
+    name: str
     raw: str
     data: Dict[str, str]
     text: str
@@ -19,42 +20,54 @@ class ReceivedTestData:
 
 @pytest.mark.parametrize('data', [
     ReceivedTestData(
-        '{"text": "Hello World", "file": "test.py", "formatText": "1"}',
-        {'text': 'Hello World', 'file': 'test.py', 'formatText': '1'},
-        'Hello World',
-        'test.py',
-        True
+        name='Valid data with formatText as "1"',
+        raw='{"text": "Hello World", "file": "test.py", "formatText": "1"}',
+        data={'text': 'Hello World', 'file': 'test.py', 'formatText': '1'},
+        text='Hello World',
+        file='test.py',
+        format_text=True
     ),
     ReceivedTestData(
-        '{"text": "Hello World", "file": "test.py", "formatText": "0"}',
-        {'text': 'Hello World', 'file': 'test.py', 'formatText': '0'},
-        'Hello World',
-        'test.py',
-        False
+        name='Valid data with formatText as "0"',
+        raw='{"text": "Hello World", "file": "test.py", "formatText": "0"}',
+        data={'text': 'Hello World', 'file': 'test.py', 'formatText': '0'},
+        text='Hello World',
+        file='test.py',
+        format_text=False
     ),
     ReceivedTestData(
-        '{"text": "Hello World", "file": ""}',
-        {'text': 'Hello World', 'file': '', 'formatText': '1'},
-        'Hello World',
-        '',
-        True
+        name='Valid data with wrong formatText',
+        raw='{"text": "Hello World", "formatText": "None"}',
+        data={'text': 'Hello World', 'file': '', 'formatText': 'None'},
+        text='Hello World',
+        file='',
+        format_text=True
     ),
     ReceivedTestData(
-        '{"text": "Hello World"}',
-        {'text': 'Hello World', 'file': '', 'formatText': '1'},
-        'Hello World',
-        '',
-        True
+        name='Validata data with missing file',
+        raw='{"text": "Hello World", "file": ""}',
+        data={'text': 'Hello World', 'file': '', 'formatText': '1'},
+        text='Hello World',
+        file='',
+        format_text=True
     ),
     ReceivedTestData(
-        '{"text": "",',
-        {'text': '', 'file': '', 'formatText': '1'},
-        '',
-        '',
-        True
+        name='Validata data only text',
+        raw='{"text": "Hello World"}',
+        data={'text': 'Hello World', 'file': '', 'formatText': '1'},
+        text='Hello World',
+        file='',
+        format_text=True
+    ),
+    ReceivedTestData(
+        name='Invalid data with missing text',
+        raw='{"text": "",',
+        data={'text': '', 'file': '', 'formatText': '1'},
+        text='',
+        file='',
+        format_text=True
     )
-
-])
+], ids=lambda data: data.name)
 def test_received_data(data: ReceivedTestData):
     received = ReceivedData(data.raw)
 
