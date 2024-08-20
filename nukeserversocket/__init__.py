f75f2536dc1b7d72e94b40dd2837b1b34231a05b
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2024-08-20 16:56:01 -0400

    .

--- nukeserversocket/__init__.py
+++ nukeserversocket/__init__.py
@@ -1,10 +1,6 @@
 """Module will initialize the logging system and import Nuke."""
 from __future__ import annotations
 
-from . import *
-import os
 from .main import NukeServerSocket
 from .version import __version__
 from .controllers.nuke import install_nuke
-
-print('main__init__.', os.listdir())
