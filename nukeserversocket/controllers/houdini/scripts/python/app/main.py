139e1d40dfa6bdeb0b463201f8709beb6b6e0513
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2024-11-16 23:10:37 -0500

    Add houdini controller

---
+++ nukeserversocket/controllers/houdini/scripts/python/app/main.py
@@ -0,0 +1,19 @@
+"""Nuke-specific plugin for the NukeServerSocket."""
+from __future__ import annotations
+
+from nukeserversocket.main import NukeServerSocket
+from nukeserversocket.utils import stdoutIO
+from nukeserversocket.received_data import ReceivedData
+from nukeserversocket.editor_controller import BaseController
+
+
+class HoudiniController(BaseController):
+    def execute(self, data: ReceivedData) -> str:
+        with stdoutIO() as s:
+            exec(data.text, globals())
+            return s.getvalue()
+
+
+class HoudiniEditor(NukeServerSocket):
+    def __init__(self, parent=None):
+        super().__init__(HoudiniController(), parent)
